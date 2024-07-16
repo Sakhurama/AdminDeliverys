511dcfa5ed2dcc4bf664a21b644cd6084a56476e
Daniel Casita <legendadtwo@gmail.com>
2024-07-15 22:08:13 -0500

    Error del responsive solucionado en la vista productos, faltan las demás tablas
    El objetivo es que el scroll esté solamente en la tabla y no en todo el site, que el menú header esté quieto y no crezca conforme al contenido

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -10,8 +10,8 @@ const Header = (props: {
   setSidebarOpen: (arg0: boolean) => void;
 }) => {
   return (
-    <header className="sticky top-0 z-999 flex w-full max-w-full bg-white drop-shadow-1 dark:bg-boxdark dark:drop-shadow-none">
-      <div className="flex flex-grow items-center justify-between px-4 py-4 shadow-2 md:px-6 2xl:px-11 w-screen">
+    <header className="sticky top-0 z-999 flex w-full bg-white drop-shadow-1 dark:bg-boxdark dark:drop-shadow-none">
+      <div className="flex flex-grow items-center justify-between px-4 py-4 shadow-2 md:px-6 2xl:px-11">
         <div className="flex items-center gap-2 sm:gap-4 lg:hidden">
           {/* <!-- Hamburger Toggle BTN --> */}
           <button
@@ -56,7 +56,7 @@ const Header = (props: {
           </button>
           {/* <!-- Hamburger Toggle BTN --> */}
 
-          <Link className="block flex-shrink-0 lg:hidden" href="#">
+          <Link className="block flex-shrink-0 lg:hidden" href="/">
             <Image
               width={32}
               height={32}
@@ -107,7 +107,9 @@ const Header = (props: {
             {/* <!-- Dark Mode Toggler --> */}
             {/* <DarkModeSwitcher /> */}
             {/* <!-- Dark Mode Toggler --> */}
-            {/* <DarkModeSwitcher /> */}
+
+            {/* <!-- Notification Menu Area --> */}
+            <DropdownNotification />
             {/* <!-- Notification Menu Area --> */}
 
             {/* <!-- Chat Notification Area --> */}
