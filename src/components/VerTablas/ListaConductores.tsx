c8fd8129f7ed3e679b66503780bab2fa257d4cfb
Daniel Casita <legendadtwo@gmail.com>
2024-07-15 22:00:04 -0500

    Revisando responsive del header

--- src/components/VerTablas/ListaConductores.tsx
+++ src/components/VerTablas/ListaConductores.tsx
@@ -44,7 +44,7 @@ const ListaConductores = () => {
   return (
     <div className="rounded-sm border border-stroke bg-white px-5 pb-2.5 pt-6 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
       <div className="max-w-full overflow-x-auto">
-        <table className="w-full table-auto">
+        <table className="w-full min-w-max table-auto">
           <thead>
             <tr className="bg-gray-2 text-left dark:bg-meta-4">
               <th className="min-w-15 px-4 py-4 font-medium text-black dark:text-white xl:pl-11">
