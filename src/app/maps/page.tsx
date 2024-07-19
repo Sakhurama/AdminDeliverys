32b0381d1da1a2223cadd8c10b1c253c7925e6d9
Daniel Casita <legendadtwo@gmail.com>
2024-07-18 22:14:06 -0500

    Mapa genera ruta entre dos ubicaciones
    Buscador y autocompletado de lugares limitado a Colombia, la ubicación
    inicial es la posición del usuario.

--- src/app/maps/page.tsx
+++ src/app/maps/page.tsx
@@ -1,12 +1,18 @@
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
 import GoogleMaps from "@/components/GoogleMaps";
 import React from "react";
+import Script from "next/script";
 
 
 
 const MapsPage = () => {
 return (
     <DefaultLayout>
+        {/* <Script
+            defer
+            src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY}&libraries=places&callback=Medusa`}
+            strategy="afterInteractive"
+            /> */}
         <GoogleMaps/>
     </DefaultLayout>
     );
--- src/components/GoogleMaps/index.tsx
+++ src/components/GoogleMaps/index.tsx
@@ -1,45 +1,132 @@
 "use client"
 
-import React from 'react';
-import { GoogleMap, useLoadScript, MarkerF} from "@react-google-maps/api";
+import React, { Component, use, useState, useEffect } from 'react';
+import { GoogleMap, useLoadScript, MarkerF, DirectionsService, LoadScript, Autocomplete, Polyline, DirectionsRenderer } from "@react-google-maps/api";
+import MapProvider from "../MapProvider"; 
+import { count } from 'console';
 
 
-const libraries = ['places'];
+
 const mapContainerStyle = {
-  with: '100vw',
+  with: '100%',
   height: '78vh',
 };
+
 const center = {
-  lat: 4.5709,
-  lng: -74.2973,
+  lat: 5.0689, 
+  lng: -75.5174,
 };
+
 const options = {
-  disableDefaultUI: true, // Deshabilita la interfaz de usuario predeterminada
-  zoomControl: true       // Habilita el control de zoom
+  disableDefaultUI: false, // Deshabilita la interfaz de usuario predeterminada
+  zoomControl: true,       // Habilita el control de zoom
+};
+
+const autocompleteOptions = {
+  componentRestrictions: { country: "co"}, // Restringe la búsqueda a Colombia
 };
 
 
 
 const GoogleMaps = () => {
-  const {isLoaded, loadError} = useLoadScript({
-    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY as string,
-    libraries: libraries as any,
-  });
 
-  if (loadError) return <div>Error al cargar el mapa...</div>;
-  if (!isLoaded) return <div>Cargando...</div>
-  if (isLoaded) {console.log("Si funciona la logica de control de errores")}
+  const [searchResult1, setSearchResult1] = useState<google.maps.places.Autocomplete>()
+  const [searchResult2, setSearchResult2] = useState<google.maps.places.Autocomplete>()
+  const [directions, setDirections] = useState<any>(null);
+  const [location1, setLocation1] = useState<any>(null);
+  const [location2, setLocation2] = useState<any>(null);
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+        const { latitude, longitude } = position.coords;
+        setLocation1({ lat: latitude, lng: longitude });
+      }, (error) => {
+        console.log("Error obteniendo la ubicación del usuario", error);
+      });
+    }
+  }, []);
+
+  function onLoad1(autocomplete: google.maps.places.Autocomplete) {
+    setSearchResult1(autocomplete);
+  }
+
+  function onLoad2(autocomplete: google.maps.places.Autocomplete) {
+    setSearchResult2(autocomplete);
+  }
+
+  function locationSelected1() {
+    if(searchResult1) {
+      const place = searchResult1.getPlace();
+      setLocation1(place.geometry?.location!);
+    } 
+  }
+
+  function locationSelected2() {
+    if(searchResult2) {
+      const place = searchResult2.getPlace();
+      setLocation2(place.geometry?.location!);
+    } 
+  }
+
+  function directionsCallback(response: any) {
+    setDirections(response);
+  }
 
 
   return (
-    <GoogleMap
-    mapContainerStyle={mapContainerStyle}
-    center={center}
-    zoom={8}
-    options={options}
-    >
-      <MarkerF position={center} />
-    </GoogleMap>
+    <MapProvider>
+      <div>
+        <div>
+          <Autocomplete
+                onLoad = {onLoad1}
+                onPlaceChanged={locationSelected1}
+                fields={['geometry.location', 'formatted_address']}
+                options={autocompleteOptions}
+          >
+                <input 
+                  type="text"
+                  placeholder="Ingrese una dirección..."
+                  className='mb-4 p-4 border-2 border-gray-300 rounded-lg w-150 text-black'
+                  />
+          </Autocomplete>
+          <Autocomplete
+                onLoad = {onLoad2}
+                onPlaceChanged={locationSelected2}
+                fields={['geometry.location', 'formatted_address']}
+                options={autocompleteOptions}
+          >
+                <input 
+                  type="text"
+                  placeholder="Ingrese una dirección..."
+                  className='mb-4 p-4 border-2 border-gray-300 rounded-lg w-150 text-black'
+                  />
+          </Autocomplete>
+        </div>
+        <GoogleMap
+          mapContainerStyle={mapContainerStyle}
+          center={location1 || center}
+          zoom={8}
+          options={options}
+        >
+         {location ? <MarkerF position={location1} /> : null}
+         {location ? <MarkerF position={location2} /> : null}
+         {location1 && location2 ? <><DirectionsService
+          options={
+            {
+              origin: location1,
+              destination: location2,
+              travelMode: google.maps.TravelMode.DRIVING,
+            }
+          }
+          callback={directionsCallback}
+        /><DirectionsRenderer 
+        directions={directions} 
+        /></> : null}
+        </GoogleMap>
+      </div>
+    </MapProvider>
   );
 };
 
