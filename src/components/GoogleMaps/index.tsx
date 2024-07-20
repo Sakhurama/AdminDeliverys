370c053d7434ed5afb0687c790a36841dbec4327
Daniel Casita <legendadtwo@gmail.com>
2024-07-20 09:34:16 -0500

    Marker rota de posición - Posiciones aleatorias

--- src/components/GoogleMaps/index.tsx
+++ src/components/GoogleMaps/index.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { Component, use, useState, useEffect } from 'react';
+import React, { Component, use, useState, useEffect, useCallback } from 'react';
 import { GoogleMap, useLoadScript, MarkerF, DirectionsService, LoadScript, Autocomplete, Polyline, DirectionsRenderer } from "@react-google-maps/api";
 import MapProvider from "../MapProvider"; 
 import { count } from 'console';
@@ -28,13 +28,36 @@ const autocompleteOptions = {
 
 
 
+
 const GoogleMaps = () => {
 
   const [searchResult1, setSearchResult1] = useState<google.maps.places.Autocomplete>()
   const [searchResult2, setSearchResult2] = useState<google.maps.places.Autocomplete>()
+  const [driverPosition, setDriverPosition] = useState({
+    position: { lat: 40.7128, lng: -74.0060 },
+    heading: 0
+  });
+  const [mapsLoaded, setMapsLoaded] = useState<any>(null);
+  const [icon, setIcon] = useState<any>(null);
   const [directions, setDirections] = useState<any>(null);
   const [location1, setLocation1] = useState<any>(null);
   const [location2, setLocation2] = useState<any>(null);
+
+  const danielito = useCallback((map: google.maps.Map) => {
+    const createIcon = (heading: number) => ({
+      path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
+      scale: 5,
+      fillColor: "#FF0000",
+      fillOpacity: 1,
+      strokeWeight: 2,
+      rotation: heading
+    });
+    
+    setIcon(createIcon(driverPosition!.heading));
+    setMapsLoaded(google.maps);
+  }, []);
+
+
 
   useEffect(() => {
     if (navigator.geolocation) {
@@ -47,6 +70,47 @@ const GoogleMaps = () => {
       });
     }
   }, []);
+
+  useEffect(() => {
+    const updateDriverPosition = () => {
+      setDriverPosition(prevState => {
+        const newLat = prevState.position.lat + (Math.random() - 0.5) * 0.001;
+        const newLng = prevState.position.lng + (Math.random() - 0.5) * 0.001;
+        const newHeading = Math.random() * 360; // Orientación aleatoria entre 0 y 360 grados
+
+        // Actualizar el icono con la nueva orientación
+        // setIcon(createIcon(newHeading));
+
+        if (mapsLoaded) {
+          setIcon(createIcon(newHeading));
+        }     
+        return {
+          position: { lat: newLat, lng: newLng },
+          heading: newHeading
+        };
+      });
+
+      setTimeout(updateDriverPosition, 1000);
+    };
+    
+    const createIcon = (heading: number): google.maps.Symbol => ({
+      path: mapsLoaded.SymbolPath.FORWARD_CLOSED_ARROW,
+      scale: 5,
+      fillColor: "#FF0000",
+      fillOpacity: 1,
+      strokeWeight: 2,
+      rotation: heading
+    });
+
+
+    updateDriverPosition();
+
+    // return () => clearTimeout(updateDriverPosition);
+  }, [mapsLoaded]);
+
+  useEffect(() => {
+    console.log(icon);
+  }, [icon]);
 
   function onLoad1(autocomplete: google.maps.places.Autocomplete) {
     setSearchResult1(autocomplete);
@@ -85,11 +149,14 @@ const GoogleMaps = () => {
                 fields={['geometry.location', 'formatted_address']}
                 options={autocompleteOptions}
           >
-                <input 
-                  type="text"
-                  placeholder="Ingrese una dirección..."
-                  className='mb-4 p-4 border-2 border-gray-300 rounded-lg w-150 text-black'
-                  />
+            <div className="flex items-center mb-4">
+              <svg  xmlns="http://www.w3.org/2000/svg"  width="30"  height="30"  viewBox="0 0 24 24"  fill="#FFBC0D"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M18.364 4.636a9 9 0 0 1 .203 12.519l-.203 .21l-4.243 4.242a3 3 0 0 1 -4.097 .135l-.144 -.135l-4.244 -4.243a9 9 0 0 1 12.728 -12.728zm-6.364 3.364a3 3 0 1 0 0 6a3 3 0 0 0 0 -6z" /></svg>
+              <input 
+                type="text"
+                placeholder="Ingrese una dirección..."
+                className="p-4 ml-4 border-2 border-gray-300 border-[#FFBC0D] rounded-lg w-150 text-black"
+              />
+            </div>
           </Autocomplete>
           <Autocomplete
                 onLoad = {onLoad2}
@@ -100,18 +167,20 @@ const GoogleMaps = () => {
                 <input 
                   type="text"
                   placeholder="Ingrese una dirección..."
-                  className='mb-4 p-4 border-2 border-gray-300 rounded-lg w-150 text-black'
+                  className='mb-4 p-4 border-2 border-gray-300 border-[#DA291C] rounded-lg w-150 text-black'
                   />
           </Autocomplete>
         </div>
         <GoogleMap
+          onLoad={danielito}
           mapContainerStyle={mapContainerStyle}
-          center={location1 || center}
-          zoom={8}
+          center={driverPosition.position || center}
+          zoom={18}
           options={options}
         >
-         {location ? <MarkerF position={location1} /> : null}
-         {location ? <MarkerF position={location2} /> : null}
+         {driverPosition && icon ? <MarkerF position={driverPosition.position} icon={icon}/> : null}
+         {/* {location ? <MarkerF position={location1} /> : null} */}
+         {/* {location ? <MarkerF position={location2} /> : null}
          {location1 && location2 ? <><DirectionsService
           options={
             {
@@ -123,7 +192,7 @@ const GoogleMaps = () => {
           callback={directionsCallback}
         /><DirectionsRenderer 
         directions={directions} 
-        /></> : null}
+        /></> : null} */}
         </GoogleMap>
       </div>
     </MapProvider>
