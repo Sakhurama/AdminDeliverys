0a7edc9f016537843bc18a452b111d0b358badf9
Daniel Casita <legendadtwo@gmail.com>
2024-07-22 20:43:47 -0500

    Drag and drop documentación kit dnd

---
+++ src/app/paqueteDomicilios/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import DefaultLayout from '@/components/Layouts/DefaultLayout'
+import DragDrop from '@/components/DomiciliosDnD/DragDrop'
+
+const PaqueteDomicilios = () => {
+  return (
+    <DefaultLayout>
+      <DragDrop/>
+    </DefaultLayout>
+  )
+}
+
+export default PaqueteDomicilios
---
+++ src/components/DomiciliosDnD/PedidosDrag.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {useDraggable} from '@dnd-kit/core';
+import {CSS} from '@dnd-kit/utilities';
+
+export function Draggable(props:any) {
+  const {attributes, listeners, setNodeRef, transform} = useDraggable({
+    id: props.id,
+  });
+
+  const style = {
+    transform: CSS.Translate.toString(transform),
+  }
+  // const style = transform ? {
+  //   transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
+  // } : undefined;
+
+  
+  return (
+    <button ref={setNodeRef} style={style} {...listeners} {...attributes}>
+      {props.children}
+    </button>
+  );
+}
